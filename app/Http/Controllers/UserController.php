f1a986032ebccd337fe03fdc3fa21e25ac889dc1
Uriah Ahrak <uriah.achrak@gmail.com>
2018-05-15 18:29:44 +0300

    Added register and login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use App\User;
 use http\Exception;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Exceptions\JWTException;
-use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\JWTAuth;
 
 class UserController extends Controller
 {
